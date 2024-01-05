89a231b8bf1cec57433800d4f557ee3d46af98b3
Claudio Beatrice <claudi0.beatric3@gmail.com>
2024-01-05 11:57:55 +0100

    feat: implement melange-specific download logic

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for melange.
 GH_REPO="https://github.com/chainguard-dev/melange"
 TOOL_NAME="melange"
 TOOL_TEST="melange --help"
@@ -14,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if melange is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -27,12 +25,10 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if melange has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,10 +37,10 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for melange
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$GH_REPO/releases/download/v${version}/melange_${version}_$(get_platform)_$(get_arch).tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
+	echo "FILENAME -> $filename"
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
@@ -61,7 +57,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert melange executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
@@ -71,4 +66,27 @@ install_version() {
 		rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
+}
+
+get_platform() {
+  uname | tr '[:upper:]' '[:lower:]'
+}
+
+get_arch() {
+  local arch=""
+
+  case "$(uname -m)" in
+      x86_64|amd64) arch="amd64"; ;;
+      i686|i386) arch="386"; ;;
+      armv6l) arch="armv6"; ;;
+      armv7l) arch="armv7"; ;;
+      aarch64|arm64) arch="arm64"; ;;
+      ppc64le) arch="ppc64le"; ;;
+      *)
+        echo "Arch '$(uname -m)' not supported!" >&2
+        exit 1
+        ;;
+  esac
+
+  echo -n $arch
 }
